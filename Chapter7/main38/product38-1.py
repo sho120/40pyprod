581f59fdb964adcf1d1cbcbbe006d02bdd9ce4ab
DESKTOP-C2QNSMF\youn <sho801921@gmail.com>
2022-11-06 11:10:36 +0900

    11.06 커밋

---
+++ Chapter7/main38/product38-1.py
@@ -0,0 +1,45 @@
+from tkinter import Frame
+import pyaudio
+import wave
+
+from playsound import playsound
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = r'Chapter7\main38\output.wav'
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT
+                ,channels=CHANNELS
+                ,rate=RATE
+                ,input=True
+                ,frames_per_buffer=CHUNK
+)
+
+print("음성 녹음을 시작합니다.")
+
+frames = []
+
+for i in range(0, int(RATE/ CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("음성녹음을 완료하였습니다.")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close
+
+print("녹음된 파일을 재생합니다.")
+playsound(WAVE_OUTPUT_FILENAME)
